0090c68815314ff148f6bc1b209a53f31c8fdf87
Jefferson Rivera <jefersonpatino@yahoo.es>
2026-03-11 09:51:46 -0500

    feat: add DocumentTypeBadge component and integrate into detail pages

--- frontend/src/components/common/index.ts
+++ frontend/src/components/common/index.ts
@@ -3,3 +3,4 @@ export { DataTable } from './DataTable';
 export { LoadingSpinner } from './LoadingSpinner';
 export { SearchInput } from './SearchInput';
 export { PageHeader } from './PageHeader';
+export { DocumentTypeBadge } from './DocumentTypeBadge';
